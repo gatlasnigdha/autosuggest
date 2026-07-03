cce1b7008fcbaa1f8b93e1791f442787a698e9f9
snigdha <gatlasnigdha123@gmail.com>
2026-07-03 12:33:59 +0530

    nodemon

---
+++ server.js
@@ -0,0 +1,11 @@
+const  express = require('express')
+
+const app = express()
+
+const PORT = 3002;
+//web server
+app.use(express.static("frontend"))
+app.listen(PORT,function(){
+    console.log("sucessfully running at http://localhost:"+PORT)
+})
+// run with node server.js before run instal npm instal express
